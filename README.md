ec62dcad04f6e1d1519475e0c8ee439815d59271
kuaifan <aipaw@live.cn>
2019-10-02 12:30:09 +0800

    no message

---
+++ README.md
@@ -0,0 +1,67 @@
+# eeui-repair
+
+eeui app文件修复(热更新)服务端代码。
+
+## config.json
+
+> config.json配置文件:
+
+```json
+{
+    "appkey": "test",
+    "welcome_image": "",
+    "welcome_wait": 2000,
+    "welcome_skip": 1,
+    "welcome_jump": "",
+    "welcome_limit_s": 0,
+    "welcome_limit_e": 0
+}
+```
+
+> config.json配置文件说明:
+
+| 属性名           | 类型     | 描述                          | 默认值     |
+| ------------- | ------ | -------------------------- | ------- |
+| appkey |`String`  | [配置文件](https://eeui.app/guide/config.html)中的appkey           | -       |
+| welcome_image |`String`  | APP闪屏广告图片地址。例如：http://abc.com/welcome.png （留空不显闪屏广告）           | -       |
+| welcome_wait |`Number`  | APP闪屏广告等待时间。时间单位：毫秒           | 2000       |
+| welcome_skip |`Number`  | APP闪屏广告是否显示跳过等待。1：是、0：否           | 1       |
+| welcome_jump |`String`  | APP闪屏广告点击打开的js页面路径。如：ad.js 或 https://abc.com/ad.js （留空点击无效）           | -       |
+| welcome_limit_s |`Number`  | APP闪屏广告有效开始时限。单位：10位时间戳 （留空不限制）           | 0       |
+| welcome_limit_e |`Number`  | APP闪屏广告有效结束时限。单位：10位时间戳 （留空不限制）           | 0       |
+
+
+## zip修复包说明
+
+```
+修复包文件路径为：zip/`{应用版本号}`/`{fileName}`.zip
+修复包配置文件路径为：zip/`{应用版本号}`/`{fileName}`.json
+```
+
+> json配置文件：
+
+```json
+{
+    "platform": "android,ios",
+    "debug": 0,
+    "valid": 1,
+    "reboot": 2,
+    "reboot_info": {
+        "title": "温馨提示",
+        "message": "已为您更新至最新版本"
+    },
+    "clear_cache": 0
+}
+```
+
+> json配置文件参数说明:
+
+| 属性名           | 类型     | 描述                          | 默认值     |
+| ------------- | ------ | -------------------------- | ------- |
+| platform |`String`  | 更新平台。支持：`android`、`ios`           | android,ios       |
+| debug |`Number`  | DEBUG版本可以收到此更新包。1：支持           | 0       |
+| valid |`Number`  | 启用状态。1：启用、0：暂停、2：撤回           | 1       |
+| reboot |`Number`  | 更新完成后。0：静默、1：自动重启、2：提示重启           | 2       |
+| reboot_info.title |`String`  | 提示重启标题           | 提示标题       |
+| reboot_info.message |`String`  | 提示重启内容           | 已为您更新至最新版本。       |
+| clear_cache |`Number`  | 更新完成后。0：保留缓存、1：清除缓存           | 0       |
---
+++ index.php
@@ -0,0 +1,262 @@
+<?php
+
+/**
+ * @param $key
+ * @param null $default
+ * @return mixed|null
+ */
+function input($key, $default = null)
+{
+    if (isset($_GET[$key])) {
+        return $_GET[$key];
+    }
+    if (isset($_POST[$key])) {
+        return $_POST[$key];
+    }
+    return $default;
+}
+
+/**
+ * @param $array
+ * @param $key
+ * @param null $default
+ * @return mixed|null
+ */
+function value($array, $key, $default = null)
+{
+    if (!is_array($array)) {
+        return $default;
+    }
+    if (isset($array[$key])) {
+        return $array[$key];
+    }
+    return $default;
+}
+
+/**
+ * @param $string
+ * @param $find
+ * @return bool
+ */
+function strExists($string, $find)
+{
+    if (!is_string($string) || !is_string($find)) {
+        return false;
+    }
+    return !(strpos($string, $find) === FALSE);
+}
+
+/**
+ * @param null $path
+ * @return string
+ */
+function url($path = null)
+{
+    $protocal = isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://';
+    if (substr($path, 0, 1) === '/') {
+        return $protocal . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '') . $path;
+    }
+    $php_self = $_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_SERVER['SCRIPT_NAME'];
+    $path_info = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+    $req_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $php_self . (isset($_SERVER['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : $path_info);
+    $curUrl = $protocal . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '') . $req_url;
+    if ($path === null) {
+        return $curUrl;
+    }
+    return dirname($curUrl) . '/' . $path;
+}
+
+/**
+ * @return array|mixed
+ */
+function getConfig()
+{
+    $array = [];
+    $file = __DIR__ . '/config.json';
+    if (is_file($file)) {
+        $array = json_decode(file_get_contents($file), true);
+    }
+    $array['appkey'] = value($array, 'appkey', 'test');
+    $array['welcome_image'] = value($array, 'welcome_image', '');
+    $array['welcome_wait'] = intval(value($array, 'welcome_wait', 2000));
+    $array['welcome_skip'] = intval(value($array, 'welcome_skip', 1));
+    $array['welcome_jump'] = value($array, 'welcome_jump', '');
+    $array['welcome_limit_s'] = intval(value($array, 'welcome_limit_s', 0));
+    $array['welcome_limit_e'] = intval(value($array, 'welcome_limit_e', 0));
+    return $array;
+}
+
+/**
+ * @param $file
+ * @return array
+ */
+function getJson($file)
+{
+    $array = [
+        'platform' => '',
+        'debug' => 0,
+        'valid' => 1,
+        'reboot' => 2,
+        'reboot_info' => [],
+        'clear_cache' => 0,
+    ];
+    $jsonFile = substr($file, 0, strlen($file) - 4) . '.json';
+    if (is_file($jsonFile)) {
+        $jsonArray = json_decode(file_get_contents($jsonFile), true);
+        $array = array_merge($array, $jsonArray);
+    }
+    if (empty($array['platform'])) {
+        $array['platform'] = 'android,ios';
+    }
+    $array['platform'] = ',' . strtolower($array['platform']) . ',';
+    $array['debug'] = intval($array['debug']);
+    $array['valid'] = intval($array['valid']);
+    $array['reboot'] = intval($array['reboot']);
+    $array['clear_cache'] = intval($array['clear_cache']);
+    if (!is_array($array['reboot_info'])) {
+        $array['reboot_info'] = [];
+    }
+    $array['reboot_info']['title'] = value($array['reboot_info'], 'title', '温馨提示');
+    $array['reboot_info']['message'] = value($array['reboot_info'], 'message', '已为您更新至最新版本');
+    $array['reboot_info']['confirm_reboot'] = 1;
+    return $array;
+}
+
+/**
+ * @param $version
+ * @param $platform
+ * @param $debug
+ * @return array
+ */
+function getUplists($version, $platform, $debug)
+{
+    $dir = 'zip/' . $version . '/';
+    $lists = glob(__DIR__ . '/' . $dir . '*.zip', GLOB_BRACE);
+    sort($lists);
+
+    $uplists = [];
+    foreach ($lists AS $key => $file) {
+        if (is_dir($file)) {
+            continue;
+        }
+        $jsonArray = getJson($file);
+        if (!strExists($jsonArray['platform'], ',' . $platform . ',')) {
+            continue;
+        }
+        if ($debug && $jsonArray['debug'] !== 1) {
+            continue;
+        }
+        if ($jsonArray['valid'] === 0) {
+            continue;
+        }
+        $fileName = basename($file);
+        $filePath = $dir . $fileName;
+        $uplists[] = [
+            'id' => base64_encode(substr($filePath, 0, strlen($filePath) - 4)),
+            'size' => sprintf("%.2f", filesize($file) / 1024),
+            'path' => url($filePath),
+            'valid' => $jsonArray['valid'],
+            'clear_cache' => $jsonArray['clear_cache'],
+            'reboot' => $jsonArray['reboot'],
+            'reboot_info' => $jsonArray['reboot_info'],
+        ];
+    }
+
+    return $uplists;
+}
+
+/**
+ * @param $msg
+ * @param array $data
+ * @param int $ret
+ */
+function error($msg, $data = [], $ret = 0)
+{
+    $param = [
+        'ret' => $ret,
+        'msg' => $msg,
+        'data' => $data
+    ];
+    header('Content-Type: application/json');
+    echo json_encode($param);
+    die();
+}
+
+/**
+ * @param $msg
+ * @param array $data
+ * @param int $ret
+ */
+function success($msg, $data = [], $ret = 1)
+{
+    $param = [
+        'ret' => $ret,
+        'msg' => $msg,
+        'data' => $data
+    ];
+    header('Content-Type: application/json');
+    echo json_encode($param);
+    die();
+}
+
+/** ************************************************************************************************/
+/** ************************************************************************************************/
+/** ************************************************************************************************/
+
+$act = input('act', 'update-lists');
+
+switch ($act) {
+    case "update-success":
+        {
+            $id = base64_decode(input('id'));
+            $path = $file = __DIR__ . '/' . $id . '.success.log';
+            file_put_contents($path, date("Y-m-d H:i:s") . "\n", FILE_APPEND);
+            success('success');
+            break;
+        }
+
+    case "update-delete":
+        {
+            $id = base64_decode(input('id'));
+            $path = $file = __DIR__ . '/' . $id . '.delete.log';
+            file_put_contents($path, date("Y-m-d H:i:s") . "\n", FILE_APPEND);
+            success('success');
+            break;
+        }
+
+    case "update-lists":
+        {
+            $appkey = input('appkey');
+            $package = input('package');
+            $version = input('version');
+            $platform = strtolower(input('platform'));
+            $debug = intval(input('debug'));
+            if (empty($appkey)) {
+                error("appkey is empty!");
+            }
+            if (empty($version)) {
+                error("version is empty!");
+            }
+            if (empty($platform)) {
+                error("version is platform!");
+            }
+            $config = getConfig();
+            if ($config['appkey'] !== $appkey) {
+                error("appkey is error!");
+            }
+            $config['uplists'] = getUplists($version, $platform, $debug);
+            success('success', $config);
+            break;
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
